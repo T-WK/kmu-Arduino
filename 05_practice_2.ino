8345f822d5a8ab02c173d006980bff5380b8885a
T-WK <66228633+T-WK@users.noreply.github.com>
2020-09-16 13:46:05 +0900

    Update 05_practice_2.ino

--- 05_practice_2.ino
+++ 05_practice_2.ino
@@ -17,7 +17,7 @@ void loop() {
   Serial.println(++count);
   toggle = toggle_state(toggle); //toggle LED value.
   digitalWrite(PIN_LED, toggle); // update LED status.
-  delay(1000); // wait for 1,000 milliseconds
+  delay(200); // wait for 1,000 milliseconds
 }
 
 int toggle_state(int toggle) {
